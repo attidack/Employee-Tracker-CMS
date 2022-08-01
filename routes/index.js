282047dc3d43393db84be357af45a80d792e33b1
Jon Bird <attid@Jons-Laptop>
2022-07-31 19:48:06 -0600

    added department and role routes

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,6 @@ const router = express.Router();
 
 router.use(require('./departmentRoutes'));
 router.use(require('./roleRoutes'));
-router.use(require('./employeeRoutes'));
+router.use(require('./empRoutes'));
 
 module.exports = router;
