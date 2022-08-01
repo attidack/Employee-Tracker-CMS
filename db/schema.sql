db84eca3cb708e8b3ebd778c404c6bd00a8c1930
Jon Bird <attid@Jons-Laptop>
2022-07-31 19:27:59 -0600

    inquirer install

--- db/schema.sql
+++ db/schema.sql
@@ -4,8 +4,8 @@ DROP TABLE IF EXISTS roles;
 
 CREATE TABLE employee (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(20) NOT NULL,
-  last_name VARCHAR(20) NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
   roles_id INTEGER,
   manager_id INTEGER,
    CONSTRAINT fk_roles_id
@@ -17,12 +17,12 @@ CREATE TABLE employee (
 
 CREATE TABLE department (
  id INT AUTO_INCREMENT PRIMARY KEY,
- name VARCHAR(20) NOT NULL
+ name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE roles (
  id INTEGER AUTO_INCREMENT PRIMARY KEY,
- title VARCHAR(20) NOT NULL,
+ title VARCHAR(30) NOT NULL,
  salary DECIMAL NOT NULL,
  department_id INTEGER NOT NULL,
  CONSTRAINT fk_department
