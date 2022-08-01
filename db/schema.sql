18defdd9d363dcc64b3690937917b7d3923092a1
Jon Bird <attid@Jons-Laptop>
2022-07-31 18:59:31 -0600

    working on db schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,40 +1,32 @@
-DROP TABLE IF EXISTS votes;
-DROP TABLE IF EXISTS candidates;
-DROP TABLE IF EXISTS parties;
-DROP TABLE IF EXISTS voters;
+DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS roles;
 
-CREATE TABLE parties (
+CREATE TABLE employee (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(50) NOT NULL,
-  description TEXT
+  first_name VARCHAR(20) NOT NULL,
+  last_name VARCHAR(20) NOT NULL,
+  roles_id INTEGER,
+  manager_id INTEGER,
+   CONSTRAINT fk_roles_id
+       FOREIGN KEY (roles_id)
+       REFERENCES roles (id)
+       ON DELETE SET NULL,
+ created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE candidates (
-  id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  party_id INTEGER,
-  industry_connected BOOLEAN NOT NULL,
-  CONSTRAINT fk_party
-    FOREIGN KEY (party_id)
-    REFERENCES parties(id)
-    ON DELETE SET NULL
+CREATE TABLE department (
+ id INT AUTO_INCREMENT PRIMARY KEY,
+ name VARCHAR(20) NOT NULL
 );
 
-CREATE TABLE voters (
-  id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  email VARCHAR(50) NOT NULL,
-  created_at DATETIME DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TABLE votes (
-  id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  voter_id INTEGER NOT NULL,
-  candidate_id INTEGER NOT NULL,
-  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-  CONSTRAINT uc_voter UNIQUE (voter_id),
-  CONSTRAINT fk_voter FOREIGN KEY (voter_id) REFERENCES voters(id) ON DELETE CASCADE,
-  CONSTRAINT fk_candidate FOREIGN KEY (candidate_id) REFERENCES candidates(id) ON DELETE CASCADE
+CREATE TABLE roles (
+ id INTEGER AUTO_INCREMENT PRIMARY KEY,
+ title VARCHAR(20) NOT NULL,
+ salary DECIMAL NOT NULL,
+ department_id INTEGER NOT NULL,
+ CONSTRAINT fk_department
+       FOREIGN KEY (department_id)
+       REFERENCES department (id)
+       ON DELETE CASCADE
 );
